cef61f82676821eccfce3c21a71107d64d847d15
DESKTOP-23FAMOH\aktbg <aktbgk@gmail.com>
2021-01-04 22:22:25 +0900

    [update]product.java에 p_subcategory_id 변수 추가

--- src/main/java/com/gumzima/shopping/model/domain/Product.java
+++ src/main/java/com/gumzima/shopping/model/domain/Product.java
@@ -4,6 +4,7 @@ package com.gumzima.shopping.model.domain;
 
 public class Product {
 	private int product_id;
+	private int p_subcategory_id;
 	private String name;
 	private int price;
 	private String content;
@@ -11,11 +12,18 @@ public class Product {
 	private int hit;
 	private int stock;
 	private String regdate;
+	
 	public int getProduct_id() {
 		return product_id;
 	}
 	public void setProduct_id(int product_id) {
 		this.product_id = product_id;
+	}
+	public int getP_subcategory_id() {
+		return p_subcategory_id;
+	}
+	public void setP_subcategory_id(int p_subcategory_id) {
+		this.p_subcategory_id = p_subcategory_id;
 	}
 	public String getName() {
 		return name;
